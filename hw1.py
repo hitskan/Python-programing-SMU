eb22eff2acf108504488a58292f2e89c04f83115
202510859황유준(컴퓨터과학전공) <153367392+hitskan@users.noreply.github.com>
2025-03-17 23:06:43 +0900

    Concept confirmation task(3/11) Last

--- hw1.py
+++ hw1.py
@@ -5,13 +5,12 @@ def get_radius(prompt):
 
 def get_circle_area(radius):
     s=radius*radius*3.14
-    message='반지름이 '+ str(radius)+'인 원의 넓이 = '+str(radius)+" X "+str(radius)+" X 3.14 = "+str(s)
-    return  message
+    return s
 
 #==================================================================
 print('시작')
 input_radius ="넓이를 구하고자 하는 원의 반지름은?"
 r_result= get_radius(input_radius)
 s_result=get_circle_area(r_result)
-print(s_result)
+print('반지름이',r_result,'인 원의넓이는=3.14 X',r_result,'X',r_result,'=',s_result)
 print('끝')
